57d846cdc290027eb5231b60e0c1187e5cfab180
dbyjar <fjralhakim@gmail.com>
2022-09-18 20:52:57 +0700

    update sign in

--- services/service/auth.ts
+++ services/service/auth.ts
@@ -3,6 +3,11 @@ import baseURL from '../index'
 
 const prefixURL = 'api/auth'
 
+interface signInTypes {
+  email: string;
+  password: string;
+}
+
 export const signUp = async (formData: any) => {
   const { data } = await axios.post(`${baseURL}/${prefixURL}/signup`, formData)
     .catch((error) => {
@@ -13,6 +18,12 @@ export const signUp = async (formData: any) => {
   return responseData?.errors ? responseData : responseData.data
 }
 
-export const signIn = async () => {
-  return null
+export const signIn = async (formData: signInTypes) => {
+  const data = await axios.post(`${baseURL}/${prefixURL}/signin`, formData)
+    .catch((error) => {
+      return error.response
+    })
+  
+  const responseData = data
+  return responseData.data
 }
