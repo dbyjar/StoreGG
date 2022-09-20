3b6656324cfe5175d1b8d44c3437ec61a041adca
dbyjar <fjralhakim@gmail.com>
2022-09-20 13:32:30 +0700

    update auth

--- components/organisms/navbar/auth.tsx
+++ components/organisms/navbar/auth.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
 import Link from 'next/link'
 
 import Cookies from 'js-cookie'
@@ -9,20 +12,33 @@ import baseURL from '../../../services/index'
 export default function Auth() {
   const [isLogin, setIsLogin] = useState(false)
   const [player, setPlayer] = useState({
-    avatar: '',
     name: '',
+    avatar: '',
   })
+
+  const toggleProfile = () => {
+    const dropdownMenu: any = document.getElementById('dropdownMenuLink')
+    dropdownMenu.classList.toggle('d-block')
+  }
+
+  const router = useRouter()
+  const onLogout = () => {
+    Cookies.remove('uglyTokenGG')
+
+    router.push('/')
+    setIsLogin(false)
+  }
 
   useEffect(() => {
     const uglyToken = Cookies.get('uglyTokenGG')
     
     if (uglyToken) {
       const token = atob(uglyToken)
-      const user = jwtDecode(token)
-      const { player } = user
+      const user: any = jwtDecode(token)
+      const dataPlayer = user.player
 
       setIsLogin(true)
-      setPlayer(player)
+      setPlayer(dataPlayer)
     }
   }, [])
 
@@ -31,20 +47,25 @@ export default function Auth() {
       <li className="nav-item my-auto dropdown d-flex">
         <div className="vertical-line d-lg-block d-none" />
         <div>
-          <a className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+          <a
+            href="#"
+            role="button"
+            className="dropdown-toggle ms-lg-40"
+            onClick={toggleProfile}
+          >
             <img 
-              src={`${baseURL}/uploads/${player.avatar}`}
-              className="rounded-circle"
               width="40"
               height="40"
+              className="rounded-circle"
               alt={player.name}
+              src={`${baseURL}/uploads/${player.avatar}`}
             />
           </a>
-          <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
+          <ul className="dropdown-menu border-0" id="dropdownMenuLink">
             <li><Link href="/member"><a className="dropdown-item text-lg color-palette-2">My Profile</a></Link></li>
             <li><Link href="/"><a className="dropdown-item text-lg color-palette-2">Wallet</a></Link></li>
             <li><Link href="/member/edit-profile"><a className="dropdown-item text-lg color-palette-2">Account Settings</a></Link></li>
-            <li><Link href="/"><a className="dropdown-item text-lg color-palette-2">Log Out</a></Link></li>
+            <li><a onClick={onLogout} className="dropdown-item text-lg color-palette-2">Log Out</a></li>
           </ul>
         </div>
       </li>
