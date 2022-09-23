658cddf16b7fb9361d961adb627dc2b180b1f970
dbyjar <fjralhakim@gmail.com>
2022-09-23 16:54:41 +0700

    update baseAPI

--- services/config.ts
+++ services/config.ts
@@ -1,8 +1,28 @@
+import Cookies from 'js-cookie'
 import axios, { AxiosRequestConfig } from 'axios'
 
-export default async function baseAPIRequest({ url, method, data }: AxiosRequestConfig) {
+interface baseAPIRequestTypes extends AxiosRequestConfig {
+  token?: boolean
+}
+
+export default async function baseAPIRequest({
+  url, method, data, token,
+}: baseAPIRequestTypes) {
+  let headers = {}
+
+  if (token) {
+    const tokenCookies = Cookies.get('uglyTokenGG')
+    
+    if (tokenCookies) {
+      const jwtToken = atob(tokenCookies)
+      headers = {
+        Authorization: `Bearer ${jwtToken}`,
+      }
+    }
+  }
+
   const response = await axios({
-    url, method, data,
+    url, method, data, headers,
   }).catch((err) => {
     return err.response
   })
